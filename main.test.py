203bf724fa4006069ff53e089059042b9dae2ce6
zuo_ji <zuo_ji@worksap.co.jp>
2020-07-07 15:55:50 +0800

    update

--- main.test.py
+++ main.test.py
@@ -2,6 +2,7 @@
 
 import unittest
 import parser
+import json
 
 
 class NamesTestCase(unittest.TestCase):
@@ -9,23 +10,33 @@ class NamesTestCase(unittest.TestCase):
     测试生成名字函数的类
     '''
 
+    def loadJson(self):
+        with open('./data.json', 'r') as fo:
+            data = json.load(fo)
+            return data
+
     def test_parse(self):
-        validate = '''
-        PaymentDebitPostProcess.transactionType == '@' \
-        && (PaymentDebitPostProcess.senderParentId == 0 || PaymentDebitPostProcess.senderParentId == null || PaymentDebitPostProcess.senderParentId == '') \
-        && PaymentDebitPostProcess.fundingTransactionTypeV2== 'H' \
-        && (PaymentDebitPostProcess.fundingStatus == 'P' || PaymentDebitPostProcess.fundingStatus == 'R') && PaymentDebitPostProcess.verticalCategoryId.indexOf('jewelry') >= 0
-        '''
-        obj = parser.parse(validate)
-        print('result is: ', obj)
-        self.maxDiff = None
-        self.assertDictEqual(obj, {
-            "PaymentDebitPostProcess.TransactionType": "@",
-            "PaymentDebitPostProcess.SenderParentId": "0",
-            "PaymentDebitPostProcess.FundingTransactionTypeV2": "H",
-            "PaymentDebitPostProcess.FundingStatus": "P",
-            "PaymentDebitPostProcess.VerticalCategoryId": "jewelry"
-        })
+        list = self.loadJson()
+        for item in list:
+            obj = parser.parse(item['validate'])
+            print('result is: ', obj)
+            self.maxDiff = None
+            self.assertDictEqual(obj, item['data'])
+
+    # def test_parse2(self):
+    #     validate='''
+    #     RDAData.source_event=='ACCTCRT'&&((RDAData.cntry_code=='C2'&&RDAData.rawHdrs_pp_geo_loc!='CN')||(RDAData.cntry_code!='C2'&&RDAData.cntry_code!=RDAData.rawHdrs_pp_geo_loc))
+    #     '''
+    #      obj = parser.parse(validate)
+    #     print('result is: ', obj)
+    #     self.maxDiff = None
+    #     self.assertDictEqual(obj, {
+    #         "PaymentDebitPostProcess.TransactionType": "@",
+    #         "PaymentDebitPostProcess.SenderParentId": "0",
+    #         "PaymentDebitPostProcess.FundingTransactionTypeV2": "H",
+    #         "PaymentDebitPostProcess.FundingStatus": "P",
+    #         "PaymentDebitPostProcess.VerticalCategoryId": "jewelry"
+    #     })
 
 
 unittest.main()
--- parser.py
+++ parser.py
@@ -5,29 +5,53 @@ import re
 # && (PaymentDebitPostProcess.fundingStatus == 'P' || PaymentDebitPostProcess.fundingStatus == 'R') && PaymentDebitPostProcess.verticalCategoryId.indexOf('jewelry') >= 0
 
 
+PATTERN_INDEXOF = r"(\w+)\.(\w+)\.indexOf\('(.*)'\)\s?>=\s?0"
+PATTERN_EQUAL = r"(\w+)\.(\w+)\s?[=,>,<]=\s?'?(.*?)'?$"
+
+
 def parse(validate):
-    arr = validate.split('&&')
-    result={}
-    for item in arr:
-        if (item.strip().startswith('(')):
-            orList = item.strip().lstrip('(').rstrip(')').split('||')
-            data=parseExpress(orList[0])
-        else:
-            data=parseExpress(item)
-        result[data[0]]=data[1]
+    result = {}
+    if '&&' in validate:
+        arr = validate.strip().lstrip('(').rstrip(')').split('&&')
+        for item in arr:
+            if (item.strip().startswith('(')):
+                dic = parse(item.strip())
+                if not dic:
+                    print('parse it null: ',item.strip())
+                    continue
+                for key in dic.keys():
+                    result[key] = dic[key]
+            else:
+                data = parseExpress(item.strip())
+
+            result[data[0]] = data[1]
+    else:
+        arr = validate.strip().lstrip('(').rstrip(')').split('||')
+        data = parseExpress(arr[0].strip())
+        result[data[0]] = data[1]
     return result
 
-def parseExpress(express):
-    data=()
-    if 'indexOf' in express.strip():
-        data = match(
-            r"(\w+)\.(\w+)\.indexOf\('(.*)'\)\s?>=\s?0", express.strip())
-    elif '==' in express:
-        data = match(r"(\w+)\.(\w+)\s?==\s'?(.*?)'?$", express.strip())
-    return data
 
+def parseExpress(express):
+    data = ()
+    obj = re.match(PATTERN_INDEXOF, express)
+    if obj:
+        return getData(obj)
+
+    obj = re.match(PATTERN_EQUAL, express)
+    if obj:
+        return getData(obj)
+    print('fail to parse: ', express)
+
+
+def getData(obj):
+    return (f"{obj.group(1)}.{upperFirst(obj.group(2))}", obj.group(3))
+
+
+def upperFirst(str):
+    return str[0:1].upper()+str[1:]
 
 
 def match(reg, str):
     obj = re.match(reg, str)
-    return (f"{obj.group(1)}.{obj.group(2)[0:1].upper()}{obj.group(2)[1:]}",obj.group(3))
+    return (f"{obj.group(1)}.{obj.group(2)[0:1].upper()}{obj.group(2)[1:]}", obj.group(3))
